1ee15e62c31f164e668c449dbbcb3bf89c98b8e2
Zharif Aziz Zulkarnain WIdodo <zharifazizzulkarnain@gmail.com>
2023-11-14 16:08:21 +0800

    hard times

--- app/Http/Controllers/PeraturanController.php
+++ app/Http/Controllers/PeraturanController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Peraturan;
+use App\Models\Peraturan; 
+use App\Models\BppProdukHukum; 
 use App\Http\Requests\StorePeraturanRequest;
 use App\Http\Requests\UpdatePeraturanRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PeraturanController extends Controller
 {
@@ -42,30 +44,125 @@ class PeraturanController extends Controller
         $tahunInput = $request->input('tahun-peraturan');
         $statusInput = $request->input('status-peraturan');
 
-        $peraturan = new Peraturan();
+        $peraturanData = Peraturan::select(
+            'tbl_peraturan.id AS id_peraturan',
+            'nomor_peraturan.id AS id_nomor',
+            'tahun_branch.id AS id_tahun',
+            'jenis_peraturan.id AS id_jenis',
+            'subjek_peraturan.id AS id_subjek',
+            'status_branch.id AS id_status',
+            'tbl_peraturan.judul_peraturan',
+            'nomor_peraturan.nomor AS nomor_peraturan',
+            'tahun_branch.tahun AS tahun_peraturan',
+            'jenis_peraturan.jenis AS jenis_peraturan',
+            'subjek_peraturan.subjek AS subjek',
+            'status_branch.status AS status_peraturan',
+            'tbl_peraturan.tgl_penetapan AS tanggal_penetapan',
+            'tbl_peraturan.tajuk_entri_pertama',
+            'tbl_peraturan.tipe_dokumen',
+            'tbl_peraturan.tmpt_penetapan AS tempat_penetapan',
+            'tbl_peraturan.tgl_penetapan',
+            'tbl_peraturan.tgl_pengundangan AS tanggal_pengundangan',
+            'tbl_peraturan.sumber',
+            'tbl_peraturan.detail_status',
+            'tbl_peraturan.bahasa',
+            'tbl_peraturan.lokasi',
+            'tbl_peraturan.bidang_hukum',
+            'tbl_peraturan.view',
+            'tbl_peraturan.download',
+            DB::raw('NULL AS catatan'),
+            DB::raw('NULL AS nama_pengarang'),
+            DB::raw('NULL AS jenis_pengarang'),
+            DB::raw('NULL AS tipe_pengarang'),
+            DB::raw('NULL AS tipe_subjek'),
+            DB::raw('NULL AS jenis_subjek'),
+            DB::raw('NULL AS file')
+        )
+        // ... (join statements)
+        ->when($searchInput, function ($query) use ($searchInput) {
+            $query->where(function ($subQuery) use ($searchInput) {
+                $subQuery->orWhere('judul_peraturan', 'like', '%' . $searchInput . '%');
+            });
+        })
+        ->when($subjekInput, function ($query) use ($subjekInput) {
+            $query->where('subjek_id', $subjekInput);
+        })
+        ->when($nomorInput, function ($query) use ($nomorInput) {
+            $query->where('nomor_id', $nomorInput);
+        })
+        ->when($jenisInput, function ($query) use ($jenisInput) {
+            $query->where('jenis_id', $jenisInput);
+        })
+        ->when($tahunInput, function ($query) use ($tahunInput) {
+            $query->where('tahun_id', $tahunInput);
+        })
+        ->when($statusInput, function ($query) use ($statusInput) {
+            $query->where('status_id', $statusInput);
+        });
 
-        $peraturanData = Peraturan::with(['getNomor', 'getJenis', 'getSubjek', 'getTahun', 'getStatus'])
-            ->when($searchInput, function ($query) use ($searchInput) {
-                $query->where(function ($subQuery) use ($searchInput) {
-                    $subQuery->orWhere('judul_peraturan', 'like', '%' . $searchInput . '%');
-                });
-            })
-            ->when($subjekInput, function ($query) use ($subjekInput) {
-                $query->where('subjek_id', $subjekInput);
-            })
-            ->when($nomorInput, function ($query) use ($nomorInput) {
-                $query->where('nomor_id', $nomorInput);
-            })
-            ->when($jenisInput, function ($query) use ($jenisInput) {
-                $query->where('jenis_id', $jenisInput);
-            })
-            ->when($tahunInput, function ($query) use ($tahunInput) {
-                $query->where('tahun_id', $tahunInput);
-            })
-            ->when($statusInput, function ($query) use ($statusInput) {
-                $query->where('status_id', $statusInput);
-            })
-            ->paginate(10);
+        $bppProdukHukumData = BppProdukHukum::select(
+            'bpp_produk_hukum.id AS id_produk',
+            DB::raw('NULL AS id_nomor'),
+            'tahun_branch.id AS id_tahun',
+            'jenis_peraturan.id AS id_jenis',
+            DB::raw('NULL AS id_subjek'),
+            'status_branch.id AS id_status',
+            'bpp_produk_hukum.judul AS judul_peraturan',
+            'bpp_produk_hukum.nomor_peraturan',
+            'tahun_branch.tahun AS tahun_peraturan',
+            'jenis_peraturan.jenis AS jenis_peraturan',
+            'bpp_produk_hukum.subjek',
+            'status_branch.status AS status_peraturan',
+            'bpp_produk_hukum.tanggal_penetapan',
+            DB::raw('NULL AS tajuk_entri_pertama'),
+            'bpp_produk_hukum.tipe_dokumen',
+            'bpp_produk_hukum.tempat_penetapan',
+            'bpp_produk_hukum.tanggal_penetapan AS tgl_penetapan',
+            'bpp_produk_hukum.tanggal_pengundangan',
+            'bpp_produk_hukum.sumber',
+            DB::raw('NULL AS detail_status'),
+            'bpp_produk_hukum.bahasa',
+            DB::raw('NULL AS lokasi'),
+            DB::raw('NULL AS bidang_hukum'),
+            'bpp_produk_hukum.dilihat AS view',
+            'bpp_produk_hukum.diunduh AS download',
+            'bpp_produk_hukum.catatan',
+            'bpp_produk_hukum.nama_pengarang',
+            'bpp_produk_hukum.jenis_pengarang',
+            'bpp_produk_hukum.tipe_pengarang',
+            'bpp_produk_hukum.tipe_subjek',
+            'bpp_produk_hukum.jenis_subjek',
+            'bpp_produk_hukum.file'
+        )
+        // ... (join statements)
+        ->when($searchInput, function ($query) use ($searchInput) {
+            $query->where(function ($subQuery) use ($searchInput) {
+                $subQuery->orWhere('judul', 'like', '%' . $searchInput . '%');
+            });
+        })
+        // ->when($subjekInput, function ($query) use ($subjekInput) {
+        //     // Adjust the field name based on your actual structure
+        //     $query->where('subjek', $subjekInput);
+        // })
+        // ->when($nomorInput, function ($query) use ($nomorInput) {
+        //     // Adjust the field name based on your actual structure
+        //     $query->where('nomor_peraturan', $nomorInput);
+        // })
+        ->when($jenisInput, function ($query) use ($jenisInput) {
+            // Adjust the field name based on your actual structure
+            $query->where('jenis_id', $jenisInput);
+        })
+        ->when($tahunInput, function ($query) use ($tahunInput) {
+            // Adjust the field name based on your actual structure
+            $query->where('tahun_id', $tahunInput);
+        })
+        ->when($statusInput, function ($query) use ($statusInput) {
+            // Adjust the field name based on your actual structure
+            $query->where('status_id', $statusInput);
+        });
+
+        // Unionize the results
+        $combinedData = $peraturanData->union($bppProdukHukumData)->paginate(10);
 
         // GROUPING EACH ONE OF THE TABLES
         $groupNomor = $peraturan->LatestPeraturan()->groupBy('id_nomor')->pluck('0.nomor_peraturan', '0.id_nomor');
@@ -76,7 +173,7 @@ class PeraturanController extends Controller
 
         return view('pages.produk-hukum.peraturan', [
             'title' => 'Peraturan | JDIH BPK',
-            'peraturanData' => $peraturanData,
+            'peraturanData' => $combinedData,
             'groupNomor' => $groupNomor,
             'groupTahun' => $groupTahun,
             'groupJenis' => $groupJenis,
@@ -84,6 +181,8 @@ class PeraturanController extends Controller
             'groupStatus' => $groupStatus,
         ]);
     }
+
+
 
 
     /**
